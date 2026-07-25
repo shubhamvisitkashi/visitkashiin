8ff8b4f8e83731677b964bfcba77d0dff1c563b8
shubhamvisitkashi <shubhmishraofficial@gmail.com>
2026-07-25 23:47:46 +0530

    remove techup

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -88,12 +88,6 @@
 
             @yield('content')
 
-            <footer
-                class="footer d-flex flex-column flex-md-row align-items-center justify-content-between px-4 py-3 border-top small">
-                <p class="text-muted mb-1 mb-md-0">Copyright © {{ date('Y') }} <a
-                        href="https://www.techuptechnologies.com/" target="_blank">Techup Technologies</a>.</p>
-            </footer>
-
         </div>
 
     </div>
